e58b050f19ef1941e9a540e74e316925569560f2
mslangford <415langford@gmail.com>
2017-03-10 16:35:09 -0800

    deleted

--- Kudos-login.cmd
+++
@@ -1,1 +0,0 @@
-curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{\"enrollId\": \"WebAppAdmin\",\"enrollSecret\": \"4faeaaec73\"}" "https://19cac554460e4b73b7acacf31ccfbedb-vp0.us.blockchain.ibm.com:5003/registrar"
--- read-meta.cmd
+++
@@ -1,1 +0,0 @@
-curl http://172.26.22.84:8060/medispan_poc/medispan_mod.xsodata/$metadata
